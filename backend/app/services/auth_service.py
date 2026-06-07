3badf606c556322d8c4fb34a4f180c6f91c73bbf
Juliana <ju_snascimento@outlook.com>
2026-06-07 17:46:37 -0300

    fix(auth_service): corrigindo secret_key

--- backend/app/services/auth_service.py
+++ backend/app/services/auth_service.py
@@ -12,7 +12,7 @@ from app.models.schemas import UserCreate
 
 load_dotenv()
 
-SECRET_KEY = os.getenv("SECRET_KEY", "changeme")
+SECRET_KEY = os.getenv("SECRET_KEY", "secret_key")
 ALGORITHM = os.getenv("ALGORITHM", "HS256")
 ACCESS_TOKEN_EXPIRE_MINUTES = int(os.getenv("ACCESS_TOKEN_EXPIRE_MINUTES", "10080"))
 
